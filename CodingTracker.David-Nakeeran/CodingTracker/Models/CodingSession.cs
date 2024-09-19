21ab3c21266d0ab917fdbc837c833a075a4780bf
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-09-19 11:38:32 +0100

    Added CodingSession model class

---
+++ CodingTracker.David-Nakeeran/CodingTracker/Models/CodingSession.cs
@@ -0,0 +1,9 @@
+namespace CodingTracker.Models;
+
+class CodingSession
+{
+    internal int Id { get; set; }
+    internal DateTime StartTime { get; set; }
+    internal DateTime EndTime { get; set; }
+    internal TimeSpan Duration { get; set; }
+}
