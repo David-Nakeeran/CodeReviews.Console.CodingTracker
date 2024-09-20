30472af793e79ac78b9f98cf835867a177bc6e8c
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-09-20 13:02:33 +0100

    Looping through List returned by LoadCodingSessionDataFromDb - codingSessionTracker method AddSession, adds each record to codingSessionTracker list.

--- CodingTracker.David-Nakeeran/CodingTracker/Program.cs
+++ CodingTracker.David-Nakeeran/CodingTracker/Program.cs
@@ -1,10 +1,18 @@
 ﻿using CodingTracker.Database;
+using CodingTracker.Utilities;
 namespace CodingTracker;
 class Program
 {
     static void Main(string[] args)
     {
         DatabaseManager databaseManager = new DatabaseManager();
+        CodingSessionTracker codingSessionTracker = new CodingSessionTracker();
         databaseManager.CreateDatabaseTable();
+
+        var sessions = databaseManager.LoadCodingSessionDataFromDb();
+        foreach (var session in sessions)
+        {
+            codingSessionTracker.AddSession(session);
+        }
     }
 }
