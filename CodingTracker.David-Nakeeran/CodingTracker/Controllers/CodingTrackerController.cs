c2ec42609bc0146603a92009f5d4bce4635a074d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-09-24 12:13:20 +0100

    Updated GetTimeInput method - Implemented two methods for validation checks     - CheckInputNullOrWhitespace     - IsTimeValid - Method returns validated string

--- CodingTracker.David-Nakeeran/CodingTracker/Controllers/CodingTrackerController.cs
+++ CodingTracker.David-Nakeeran/CodingTracker/Controllers/CodingTrackerController.cs
@@ -1,17 +1,26 @@
+using CodingTracker.Utilities;
 using Spectre.Console;
 
 namespace CodingTracker.Controller;
 
 class CodingTrackerController
 {
+    private readonly Validation _validation;
     // Ask user to insert start time in specific format
     // Ask user to insert end time in a specific format
     // GetTimeInput method return string
+    internal CodingTrackerController(Validation validation)
+    {
+        _validation = validation;
+    }
     internal string GetTimeInput()
     {
         var timeInput = AnsiConsole.Ask<string>("Please enter the time in the format of hh:mm or enter 0 to return to main menu");
-        // CheckInputNullOrWhitespace method return string
-        // IsTimeValid method return bool
+        timeInput = _validation.CheckInputNullOrWhitespace("Please enter the time in the format of hh:mm or enter 0 to return to main menu", timeInput);
+        while (!_validation.IsTimeValid(timeInput))
+        {
+            timeInput = AnsiConsole.Ask<string>("Please enter the time in the format of hh:mm or enter 0 to return to main menu");
+        }
         return timeInput;
     }
 
