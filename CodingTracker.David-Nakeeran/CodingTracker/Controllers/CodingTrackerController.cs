a39ea68abf5788e3049060582fc813aea55654f7
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-09-25 12:40:06 +0100

    Added new fields and updated constructor - Two new fields:     - _inputHandler     - _userInput

--- CodingTracker.David-Nakeeran/CodingTracker/Controllers/CodingTrackerController.cs
+++ CodingTracker.David-Nakeeran/CodingTracker/Controllers/CodingTrackerController.cs
@@ -1,4 +1,5 @@
 using CodingTracker.Utilities;
+using CodingTracker.Views;
 using Spectre.Console;
 
 namespace CodingTracker.Controller;
@@ -6,24 +7,23 @@ namespace CodingTracker.Controller;
 class CodingTrackerController
 {
     private readonly Validation _validation;
-    // Ask user to insert start time in specific format
-    // Ask user to insert end time in a specific format
-    // GetTimeInput method return string
-    internal CodingTrackerController(Validation validation)
+    private readonly InputHandler _inputHandler;
+    private readonly UserInput _userInput;
+    internal CodingTrackerController(Validation validation, InputHandler inputHandler, UserInput userInput)
     {
         _validation = validation;
+        _inputHandler = inputHandler;
+        _userInput = userInput;
     }
     internal string GetTimeInput()
     {
         var timeInput = AnsiConsole.Ask<string>("Please enter the time in the format of hh:mm or enter 0 to return to main menu");
         timeInput = _validation.CheckInputNullOrWhitespace("Please enter the time in the format of hh:mm or enter 0 to return to main menu", timeInput);
+        // _inputhandler.IsInputZero(string input)
         while (!_validation.IsTimeValid(timeInput))
         {
-            timeInput = AnsiConsole.Ask<string>("Please enter the time in the format of hh:mm or enter 0 to return to main menu");
+            timeInput = AnsiConsole.Ask<string>("Please enter the time in the format of hh:mm");
         }
         return timeInput;
     }
-
-    // IsTimeValid method return bool
-    // CheckInputNullOrWhitespace method return string
 }
