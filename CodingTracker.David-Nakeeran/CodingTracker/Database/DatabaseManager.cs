b83cdadfb06d766b1a5b705e0943f6f45e3e6086
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-09-26 12:06:39 +0100

    Implemented Conversion methods - Added StringToTime method to allow use with CalculateDuration method to work out duration between startTime and endTime. - Also added TimeSpanToString to convert input, ready to be inserted into database

--- CodingTracker.David-Nakeeran/CodingTracker/Database/DatabaseManager.cs
+++ CodingTracker.David-Nakeeran/CodingTracker/Database/DatabaseManager.cs
@@ -1,6 +1,7 @@
 using System.Configuration;
 using CodingTracker.Controller;
 using CodingTracker.Models;
+using CodingTracker.Utilities;
 using Dapper;
 using Microsoft.Data.Sqlite;
 using Spectre.Console;
@@ -9,9 +10,13 @@ namespace CodingTracker.Database;
 internal class DatabaseManager
 {
     private readonly CodingTrackerController _controller;
-    internal DatabaseManager(CodingTrackerController controller)
+    private readonly TimeCalculator _timeCalculator;
+    private readonly Conversion _conversion;
+    internal DatabaseManager(CodingTrackerController controller, TimeCalculator timeCalculator, Conversion conversion)
     {
         _controller = controller;
+        _timeCalculator = timeCalculator;
+        _conversion = conversion;
     }
     internal void CreateDatabaseTable()
     {
@@ -57,7 +62,14 @@ internal class DatabaseManager
         string startTime = _controller.GetTimeInput();
         AnsiConsole.MarkupLine("Please enter the end time");
         string endTime = _controller.GetTimeInput();
-        // string duration = endTime - startTime;
+        // Conversion method - converts startTime and endTime from string to DateTime
+        DateTime timeOne = _conversion.StringToTime(startTime);
+        DateTime timeTwo = _conversion.StringToTime(endTime);
+        // Pass to CalculateDuration
+        // Convert to string, recieves TimeSpan
+        TimeSpan durationConvertToString = _timeCalculator.CalculateDuration(timeOne, timeTwo);
+        // convert duration to string with TimeSpanToString method
+        string duration = _conversion.TimeSpanToString(durationConvertToString);
         string dateOfEntry = DateTime.Today.ToString();
         // DateTime.Today then .ToString() - get the current date
         // connect to database
