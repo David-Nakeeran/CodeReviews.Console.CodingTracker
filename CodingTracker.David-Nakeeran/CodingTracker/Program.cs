c2c2d8e13664172477484320bc0086c883b51e75
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-09-26 12:03:33 +0100

    Updated object instances.

--- CodingTracker.David-Nakeeran/CodingTracker/Program.cs
+++ CodingTracker.David-Nakeeran/CodingTracker/Program.cs
@@ -7,11 +7,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        Conversion conversion = new Conversion();
+        TimeCalculator timeCalculator = new TimeCalculator();
         Validation validation = new Validation();
         InputHandler inputHandler = new InputHandler();
         UserInput userInput = new UserInput();
         CodingTrackerController codingTrackerController = new CodingTrackerController(validation, inputHandler, userInput);
-        DatabaseManager databaseManager = new DatabaseManager(codingTrackerController);
+        DatabaseManager databaseManager = new DatabaseManager(codingTrackerController, timeCalculator, conversion);
         CodingSessionTracker codingSessionTracker = new CodingSessionTracker();
 
         databaseManager.CreateDatabaseTable();
